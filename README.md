37dd3adbf23282de7e24c2c843eb9d27891f921a
Manuel Mancini <m.mancini@octotelematics.com>
2023-03-29 16:06:22 +0200

    Updated documentation and renamed code methods

--- README.md
+++ README.md
@@ -4,6 +4,28 @@ This project uses Quarkus, the Supersonic Subatomic Java Framework.
 
 If you want to learn more about Quarkus, please visit its website: https://quarkus.io/ .
 
+
+## Project Purpose
+
+This project has been realized to test the partitioning and concurrency processing of data from Kafka on Quarkus.
+The original project can be found at the following link: [Startup](https://quarkus.io/blog/getting-started-kafka/)
+
+The first problem that has to been addressed is the fact that each Kafka partition is not being processed by a specific thread on Quarkus.
+
+From [HERE](https://github.com/quarkusio/quarkus/issues/21306) we can learn that processing from multiple partitions is not something 
+that is addressed by default from Quarkus due to its generic purpose.
+What we realized is that using Multi - Multi from read-write method connected to kafka we can read in unordered mode from all topics.
+Using then the method ``.group().by()`` we can regroup the data with the same logic as it has been partitioned in the topic.
+With ``.emitOn()`` the result of the ``.group().by()`` is sent to a specific WorkerPool, so a new thread is created.
+
+The method ``.transformToMultiAndMerge()`` has been used to invoke the method that process the ``Message<String>`` of a single partition.
+
+Inside the method for the specific partition a ``.transformToMultiAndConcatenate()`` has been used to keep the ordering of the topic's partition.
+
+To check if the processing is really multi-threaded an external service is invoked that process the response with a random timing.
+
+An error handling method as been realized too ``handleMessageError``: we don't want to stop the stream processing in case of error so as explained [HERE](https://quarkus.io/blog/mutiny-failure-handling/) with this method we send the error message to a specific topic, ack the message and proceed to next message.
+
 ## Running the application in dev mode
 
 You can run your application in dev mode that enables live coding using:
@@ -51,12 +73,9 @@ If you want to learn more about building native executables, please consult http
 
 - SmallRye Reactive Messaging - Kafka Connector ([guide](https://quarkus.io/guides/kafka-reactive-getting-started)): Connect to Kafka with Reactive Messaging
 
-
-## Documentation Reference
-
-- [Startup](https://quarkus.io/blog/getting-started-kafka/)
+## Useful docs 
 - [Life-saving Stackoverflow post](https://stackoverflow.com/questions/65799846/quarkus-kafka-batch-bulk-message-consumer)
 - [Useful topic - Multi Handling](https://github.com/smallrye/smallrye-reactive-messaging/issues/804)
 - [Useful topic - Exceptions on Bluk Processing](https://github.com/smallrye/smallrye-reactive-messaging/issues/925)
-- [Firsto solution to multithread processing](https://github.com/quarkusio/quarkus/issues/21306)
+- [First solution to multithread processing](https://github.com/quarkusio/quarkus/issues/21306)
 - [Handling failure on Multi](https://quarkus.io/blog/mutiny-failure-handling/)
--- src/main/java/org/acme/MovieConsumer.java
+++ src/main/java/org/acme/MovieConsumer.java
@@ -1,10 +1,5 @@
 package org.acme;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutorService;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -26,13 +21,6 @@ public class MovieConsumer {
     
     private final Logger logger = Logger.getLogger(MovieConsumer.class);
 
-    List<ExecutorService> executors = Arrays.asList(
-        Executors.newSingleThreadExecutor(),
-        Executors.newSingleThreadExecutor(),
-        Executors.newSingleThreadExecutor(),
-        Executors.newSingleThreadExecutor()
-    );
-
     @Inject @Channel("movies-error")
     Emitter<String> emitter;
 
@@ -42,7 +30,7 @@ public class MovieConsumer {
 
     private Uni<Message<String>> handleMessageError(Message<String> message, Throwable cause){
         return Uni.createFrom().item(message)
-            //.emitOn(Infrastructure.getDefaultWorkerPool())
+            .onItem().invoke(ex -> logger.errorf("MY ERROR - %s", cause.getMessage()))
             .onItem().call(m -> {
                 return Uni.createFrom().item(m)
                     .onItem().invoke(msg -> {
@@ -50,12 +38,11 @@ public class MovieConsumer {
                         m.ack();
                     });
             })
-            //.emitOn(Infrastructure.getDefaultWorkerPool())
             .onItem().transformToUni(m -> Uni.createFrom().nullItem()) //Used to return a null item. ".continueWithNull()" return a void instead
             ;
     }
 
-    private Multi<Message<String>> consume(Multi<Message<String>> messages){
+    private Multi<Message<String>> processTopic(Multi<Message<String>> messages){
         return messages
             .onItem().transformToUniAndConcatenate(m -> {
                 IncomingKafkaRecordMetadata<Long, String> meta = m.getMetadata(IncomingKafkaRecordMetadata.class).orElseThrow();
@@ -63,7 +50,6 @@ public class MovieConsumer {
                 logger.infof("Got a movie: %d - %s - From topic %s - partition %d", meta.getKey(), m.getPayload(), meta.getTopic(), meta.getPartition());
 
                 return Uni.createFrom().item(m)
-                    //.emitOn(Infrastructure.getDefaultWorkerPool())
                     .onItem().transform(record -> {
                         Movie requestMovie = new Movie();
                         requestMovie.setId(meta.getKey());
@@ -71,14 +57,12 @@ public class MovieConsumer {
                         requestMovie.setPartition(meta.getPartition());
                         return requestMovie;
                     })
-                    //.emitOn(Infrastructure.getDefaultWorkerPool())
                     .onItem().call(rm -> {
 
                         return externalResource.touchMovies(rm)
                             .onItem().invoke(res -> rm.setTitle(res.getTitle()))
                         ;
                     })
-                    //.emitOn(Infrastructure.getDefaultWorkerPool())
                     .onItem().invoke(rm -> {
                         int value = Integer.parseInt(m.getPayload().split("_")[1]) % 7;
                         if(value % 7 == 0){
@@ -86,8 +70,6 @@ public class MovieConsumer {
                         }
                     })
                     .onItem().transform(rm -> m.withPayload(rm.getTitle()))
-                    .onFailure().invoke(ex -> logger.errorf("MY ERROR - %s", ex.getMessage()))
-                    .onFailure().invoke(ex -> m.ack())
                     .onFailure().recoverWithUni(ex -> handleMessageError(m, ex).emitOn(Infrastructure.getDefaultWorkerPool()))
                     ;
             })
@@ -105,7 +87,7 @@ public class MovieConsumer {
 
     @Incoming("movies-in")
     @Outgoing("movies-out")
-    public Multi<Message<String>> reactive(Multi<Message<String>> messages){
+    public Multi<Message<String>> consume(Multi<Message<String>> messages){
         System.out.println("START METHOD");
 
         return messages
@@ -114,7 +96,7 @@ public class MovieConsumer {
                 return meta.getKey();
             }) //We can read disordered events thanks to this code that aggregate them by key
             .emitOn(Infrastructure.getDefaultWorkerPool()) //We emit to another working thread the result so anything comes after is worked on the specific thread
-            .onItem().transformToMultiAndMerge(r -> consume(r)) //We use merge because the returned result is already ordered so we don't care if we got first the topic 1 or topic 2 etc result. The only inportant thing is thath is ordered by partition 
+            .onItem().transformToMultiAndMerge(r -> processTopic(r)) //We use merge because the returned result is already ordered so we don't care if we got first the topic 1 or topic 2 etc result. The only inportant thing is thath is ordered by partition 
             .emitOn(Infrastructure.getDefaultWorkerPool())
         ;
     }
